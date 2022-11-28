d0325c0c8d51a8eccf4710bacb4581f9cba52e91
Nordaca <76541199+Nordaca@users.noreply.github.com>
2022-11-27 18:34:10 -0600

    first commit

---
+++ src/Sound.java
@@ -0,0 +1,33 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound 
+{
+    void playMusic(String musicLocation)
+    {
+        try
+        {
+            File musicPath = new File(musicLocation);
+
+            if (musicPath.exists())
+            {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            else
+            {
+                System.out.println("couldn't find " + musicPath.toString());
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/Texture.java
@@ -0,0 +1,38 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Texture {
+    public static Texture cat = new Texture("img/textures/cat.png",64);
+    public static Texture wood = new Texture("img/textures/brown_wall.png", 64);
+    public static Texture brick = new Texture("img/textures/brick1.png", 64);
+    public static Texture bluestone = new Texture("img/textures/blue_wall.png", 64);
+    public static Texture stone = new Texture("img/textures/brick2.png", 64);
+    public static Texture frog = new Texture("img/textures/frog.png", 64);
+    public static Texture dog = new Texture("img/textures/dog.png", 128);
+
+	public int[] pixels;
+	private String loc;
+    public final int SIZE;
+
+    public Texture(String location, int size) {
+        loc = location;
+        SIZE = size;
+        pixels = new int[SIZE * SIZE];
+        load();
+    }
+
+    private void load() {
+        try {
+            BufferedImage image = ImageIO.read(new File(loc));
+            int w = image.getWidth();
+            int h = image.getHeight();
+            image.getRGB(0, 0, w, h, pixels, 0, w);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+}
